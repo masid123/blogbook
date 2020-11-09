f9ae0f7fcb4e4e9294f9c7abbfb8cf1611a8efd4
masid123 <3262359384@qq.com>
2020-11-09 22:42:46 +0800

    '数据库配置'

--- blogbook/blogbook/settings.py
+++ blogbook/blogbook/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blogbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT':'3306',
+        'USER':'django_blog',
+        'PASSWORD':'123456',
+        'NAME':'django_blog'
     }
 }
 
